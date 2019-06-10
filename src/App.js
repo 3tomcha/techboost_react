b859c20ff925d1c9ae62cbaa6d2998d1e5d03b4c
Tomoya Kobayashi <to_kobayashi@gmail.com>
2019-06-10 17:54:46 +0900

    03 - todoアプリの作成開始

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [
+        {id: 1, text: "dataを表示する"},
+        {id: 2, text: "簡単な構成を知る"}
+      ]
+    };
+  }
+  render(){
+    return(
+      <div class="App">
+        {this.state.todos.map(item => <li>{item.text}</li>)}
+      </div>
+    );
+  }
 }
 
 export default App;
