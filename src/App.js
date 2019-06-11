71750dff71e2696d55ecdca35c4c0f90ee6c299e
Tomoya Kobayashi <to_kobayashi@gmail.com>
2019-06-11 15:18:56 +0900

    functionを用いてリファクタリング

--- src/App.js
+++ src/App.js
@@ -2,62 +2,67 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Todos extends Component{
-  constructor(props){
-    super(props);
-  }
-  render(){
-    return(
-      <ul>
-      {this.props.todos.map(item => <li>{item.text}</li>)}
-      </ul>
-    );
-  }
+function Todo(props){
+  return <li>{props.key}{props.text}</li>;
+}
+
+function Todos(props){
+  return(
+    <ul>
+    {props.todos.map(item => <Todo key={item.id} text={item.text}/>)}
+    </ul>
+  );
 }
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      todos: [
-        {text: "仮カラム1です"},
-        {text: "仮カラム2です"},
-      ]
+  constructor(){
+    super();
+    const todos = [
+      {id: 1, text: "仮カラム1です"},
+      {id: 2, text: "仮カラム2です"}];
+
+      this.state = {
+        todos: todos,
+        countTodo: todos.length
+      };
+    }
+
+    handleInput = (e) => {
+      this.setState({
+        newtodos: {
+          id: this.state.countTodo + 1,
+          text: e.target.value
+        },
+      });
+    }
+
+    insertTodo = () => {
+      const todos = this.state.todos;
+      todos.push(this.state.newtodos);
+      this.setState({
+        todos: todos,
+        countTodo: todos.length
+      });
     };
+
+    deleteTodo = () => {
+      const todos = this.state.todos;
+      todos.pop();
+      this.setState({
+        todos: todos
+      });
+    };
+
+    render(){
+      return(
+        <div class="App">
+        <Todos todos={this.state.todos} />
+        <input type="text" id="todo" onChange={this.handleInput}/>
+        <button onClick={this.insertTodo}>更新する</button>
+        <button onClick={this.deleteTodo}>削除する</button>
+        </div>
+      );
+    }
   }
 
-  handleInput = (e) => {
-    this.setState({
-      newtodos: {text: e.target.value}
-    });
-  }
-
-  insertTodo = () => {
-    const todos = this.state.todos;
-    todos.push(this.state.newtodos);
-    this.setState({
-      todos: todos
-    });
-  };
-
-  deleteTodo = () => {
-    const todos = this.state.todos;
-    todos.pop();
-    this.setState({
-      todos: todos
-    });
-  };
-
-  render(){
-    return(
-      <div class="App">
-      <Todos todos={this.state.todos} />
-      <input type="text" id="todo" onChange={this.handleInput}/>
-      <button onClick={this.insertTodo}>更新する</button>
-      <button onClick={this.deleteTodo}>削除する</button>
-      </div>
-    );
-  }
-}
-
-export default App;
+  export default App;
