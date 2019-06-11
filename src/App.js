4019e724aba885a02e592d80392fbd5f58c6add1
Tomoya Kobayashi <to_kobayashi@gmail.com>
2019-06-11 11:45:02 +0900

    削除機能の実装

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Todos extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <ul>
+      {this.props.todos.map(item => <li>{item.text}</li>)}
+      </ul>
+    );
+  }
+}
+
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
       todos: [
-        {text: "dataを表示する"},
-        {text: "簡単な構成を知る"}
+        {text: "仮カラム1です"},
+        {text: "仮カラム2です"},
       ]
     };
   }
@@ -17,25 +30,31 @@ class App extends Component{
     this.setState({
       newtodos: {text: e.target.value}
     });
-    console.log(this.state.newtodos);
   }
 
   insertTodo = () => {
-    const temp = this.state.todos;
-    temp.push(this.state.newtodos);
+    const todos = this.state.todos;
+    todos.push(this.state.newtodos);
     this.setState({
-      todos: temp
+      todos: todos
+    });
+  };
+
+  deleteTodo = () => {
+    const todos = this.state.todos;
+    todos.pop();
+    this.setState({
+      todos: todos
     });
   };
 
   render(){
     return(
       <div class="App">
-      <ul>
-      {this.state.todos.map(item => <li>{item.text}</li>)}
-      </ul>
+      <Todos todos={this.state.todos} />
       <input type="text" id="todo" onChange={this.handleInput}/>
       <button onClick={this.insertTodo}>更新する</button>
+      <button onClick={this.deleteTodo}>削除する</button>
       </div>
     );
   }
